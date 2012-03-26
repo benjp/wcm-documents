ebb22393ee2409c575550cd2238b3bda51ff17e3
Benjamin Paillereau <bpaillereau@gmail.com>
2012-03-26 18:54:11 +0200

    templates modified to differentiate nav from list

--- src/main/java/org/exoplatform/ecms/dokuzu/list/package-info.java
+++ src/main/java/org/exoplatform/ecms/dokuzu/list/package-info.java
@@ -1,2 +1,11 @@
+@org.juzu.Application
+
+@Assets(
+        scripts = {
+                @Script(src = "js/jquery-1.7.1.min.js")
+        }
+)
 package org.exoplatform.ecms.dokuzu.list;
 
+import org.juzu.plugin.asset.Assets;
+import org.juzu.plugin.asset.Script;
--- src/main/java/org/exoplatform/ecms/dokuzu/navigation/package-info.java
+++ src/main/java/org/exoplatform/ecms/dokuzu/navigation/package-info.java
@@ -1,2 +1,11 @@
+@org.juzu.Application
+
+@Assets(
+        scripts = {
+                @Script(src = "js/jquery-1.7.1.min.js")
+        }
+)
 package org.exoplatform.ecms.dokuzu.navigation;
 
+import org.juzu.plugin.asset.Assets;
+import org.juzu.plugin.asset.Script;
